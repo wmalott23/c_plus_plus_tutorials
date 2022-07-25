6b0210a5a8c84d07714d7217788e9f616f0ca02a
Ashlee Malott <wesley_malott@yahoo.com>
2022-07-25 12:54:39 -0700

    practices some more syntax and printing unique characters

--- tutorials.cpp
+++ tutorials.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout << "Hello World!";
+    // ctrl forward slash works for comments which is great
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "\t tab";
+    std::cout << "\\backslash";
+    std::cout << "\"double quotes\""; // You can do comments after lines which is also great
+    // std::cout << "I am learning C++"; Commenting out lines of code is extremely useful
     return 0;
 }
